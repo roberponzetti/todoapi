a556551cd72b841249f08ea62432f6306e563573
Robertino <roberponzetti@gmail.com>
2022-02-20 18:32:54 -0300

    queries

--- src/routes/todolist.js
+++ src/routes/todolist.js
@@ -13,60 +13,60 @@ const query = `
 
 
 router.get('/', (req,res) => {
-    mysqlConnection.query('SELECT * FROM todoitems', (err,  rows, fields) => {
+    mysqlConnection.query('SELECT * FROM todoitems', (err,  rows) => {
         if(!err){
-            res.send('WELCOME!!!!');
+            res.json(rows);
         }else{
             console.log(err);
         }
     })
 });
 
-// router.get('/:itemId', (req,res) => {
-//     const { itemId } = req.params;
-//     mysqlConnection.query('SELECT * FROM todoitems WHERE itemId = ?', [itemId], (err,  rows, fields) => {
-//         if(!err){
-//             res.json(rows);
-//         }else{
-//             console.log(err);
-//         }
-//     })
-// });
+router.get('/:itemId', (req,res) => {
+    const { itemId } = req.params;
+    mysqlConnection.query('SELECT * FROM todoitems WHERE itemId = ?', [itemId], (err,  rows, fields) => {
+        if(!err){
+            res.json(rows);
+        }else{
+            console.log(err);
+        }
+    })
+});
 
-// router.post('/', (req,res) => {
-//     const { itemId, description, completed, folderId} = req.body;
-//     req.body
-//     mysqlConnection.query(query, [itemId, description, completed, folderId], (error) => {
-//         if(!error){
-//             res.json({Status: 'Item saved successfully!'});
-//         }else{
-//             console.log(error);
-//         }
-//     })
-// });
+router.post('/', (req,res) => {
+    const { itemId, description, completed, folderId} = req.body;
+    req.body
+    mysqlConnection.query(query, [itemId, description, completed, folderId], (error) => {
+        if(!error){
+            res.json({Status: 'Item saved successfully!'});
+        }else{
+            console.log(error);
+        }
+    })
+});
 
-// router.put('/:id', (req,res) => {
-//     const { description, completed, folderId } = req.body;
-//     const { itemId } = req.params;
-//     mysqlConnection.query(query, [itemId, description, completed, folderId], (error) => {
-//         if(!error){
-//             res.json({Status: 'Item updated successfully!'});
-//         }else{
-//             console.log(error);
-//         }
-//     })
-// });
+router.put('/:id', (req,res) => {
+    const { description, completed, folderId } = req.body;
+    const { itemId } = req.params;
+    mysqlConnection.query(query, [itemId, description, completed, folderId], (error) => {
+        if(!error){
+            res.json({Status: 'Item updated successfully!'});
+        }else{
+            console.log(error);
+        }
+    })
+});
 
-// router.delete('/:id', (req,res) => {
-//     const { itemId } = req.params;
-//     mysqlConnection.query('DELETE FROM items WHERE id = ?' , [itemId], (error) => {
-//         if(!error){
-//             res.json({Status: 'Item deleted successfully!'});
-//         }else{
-//             console.log(error);
-//         }
-//     })
-// });
+router.delete('/:id', (req,res) => {
+    const { itemId } = req.params;
+    mysqlConnection.query('DELETE FROM items WHERE id = ?' , [itemId], (error) => {
+        if(!error){
+            res.json({Status: 'Item deleted successfully!'});
+        }else{
+            console.log(error);
+        }
+    })
+});
 
 
 module.exports = router;
